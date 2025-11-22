a7473f8060ed2fd60661644436936c10fda6a0a4
Sergiy Lavryk <jagermesh@gmail.com>
2025-11-22 12:48:04 -0500

    fx

--- test.js
+++ test.js
@@ -2,11 +2,4 @@
 
 import MathToImageService from './index.js';
 
-let mathToImageService = new MathToImageService({
-  port: 8000,
-  redis: {
-    lifespan: '30 min',
-    connectString: 'redis://localhost:6379',
-  },
-});
-mathToImageService.start();
+(new MathToImageService()).start();
